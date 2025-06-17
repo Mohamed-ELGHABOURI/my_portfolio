0dcf2c93eae1fbfd7e55c70a1cedf232e95aa2c2
Mohamed <mohamed.el.ghabouri2003@gmail.com>
2025-06-17 19:31:20 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
+      <BrowserRouter basename="/my_portfolio">
+      {/* Vos Routes */}
+      <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
+     
     </TooltipProvider>
   </QueryClientProvider>
 );
